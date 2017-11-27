efde1e06141e4949ebe912d5d863936a13f0d2b7
JAVIER MANTILLA - Student <G00329649@gmit.ie>
2017-11-27 22:00:33 +0000

    Lab10 initial commit

--- Lab10/lab10.py
+++ Lab10/lab10.py
@@ -16,7 +16,7 @@ gray_image = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY)
 img = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2RGB)
 
 nrows = 2
-ncols = 2
+ncols = 3
 
 plt.subplot(nrows,ncols,1),plt.imshow(img,cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
@@ -44,4 +44,16 @@ imgBlur13x13 = cv2.GaussianBlur(gray_image,(KernelSizeWidth,KernelSizeHeight),0)
 # 8) Plot the 13x13 blur
 plt.subplot(nrows, ncols,4),plt.imshow(imgBlur13x13,cmap = 'gray')
 plt.title('Blur 3x3'), plt.xticks([]), plt.yticks([])
+
+# 9) Sobel edge detection
+sobelHorizontal = cv2.Sobel(imgBlur3x3,cv2.CV_64F,1,0,ksize=5)
+sobelVertical = cv2.Sobel(imgBlur3x3,cv2.CV_64F,0,1,ksize=5)
+
+# 10) Plot Sobel processed images
+plt.subplot(nrows, ncols,5),plt.imshow(sobelHorizontal,cmap = 'gray')
+plt.title('Sobel x/Blur 3x3'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(nrows, ncols,6),plt.imshow(sobelVertical,cmap = 'gray')
+plt.title('Sobel y/Blur 3x3'), plt.xticks([]), plt.yticks([])
+
 plt.show()
